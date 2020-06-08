e9af965d8effe431e13246ee5db2bfda3cc37618
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-07 23:17:12 -0400

    Updates

---
+++ precache-manifest.3f0295f0e1aa0f405a08962d81fcef13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8de9e6c5c44b6941095f1a7365d247f",
+    "url": "/Bank-of-React2/index.html"
+  },
+  {
+    "revision": "8b4a621ea5be17b4c2d8",
+    "url": "/Bank-of-React2/static/css/main.09a11714.chunk.css"
+  },
+  {
+    "revision": "8446acfcc3c6044b296a",
+    "url": "/Bank-of-React2/static/js/2.23dae534.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bank-of-React2/static/js/2.23dae534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4a621ea5be17b4c2d8",
+    "url": "/Bank-of-React2/static/js/main.52d71aef.chunk.js"
+  },
+  {
+    "revision": "77ba8a07dee7208b47b9",
+    "url": "/Bank-of-React2/static/js/runtime-main.de334f58.js"
+  }
+]);
